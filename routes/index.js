30bb8215d5c3f279599c8b802227af9137a501f2
shejacredia <56386424+shejacredia@users.noreply.github.com>
2019-12-20 15:07:27 +0800

    update

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,53 @@
 const express = require('express');
 const router = express.Router();
 const { ensureAuthenticated, forwardAuthenticated } = require('../config/auth');
-
+const User = require('../models/User');
 // Welcome Page
 router.get('/', forwardAuthenticated, (req, res) => res.render('welcome'));
+router.get("/edit", (req, res) => res.render("edit", {
+  user: req.user
+}));
+router.get("/setting", (req, res) => res.render("setting", {
+  user: req.user
+}));
+router.post("/edit", (req, res) => {
+  const {
+    name,
+    email
+  } = req.body;
+  const id = {
+    _id: req.body.id
+  };
+  const updateItem = {
+    $set: {
+      name: name,
+      email: email
+    }
+  };
+  User.updateOne(id, updateItem, (err) => {
+    if (err) {
+      console.log(err);
 
+    } else {
+      res.redirect("/setting");
+      console.log("Settings updated");
+    }
+  });
+});
+
+router.post("/delete", (req, res) => {
+  const deleteItem = req.body.id;
+  User.findByIdAndDelete(deleteItem, (err) => {
+    if (!err) {
+      console.log("Successfully deleted");
+      console.log(deleteItem);
+
+      res.redirect("/users/login");
+    } else {
+      console.log(err);
+    }
+  });
+});
 // Dashboard
 router.get('/dashboard', ensureAuthenticated, (req, res) =>
   res.render('dashboard', {
